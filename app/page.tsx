5ce74ae949fe7b4dd1312efa8c091433d062ad1b
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-08-25 17:12:14 +0700

    app(skripsi): update Hero section structure file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { Hero } from "@/components";
 const page = () => {
   return (
-    <div className="w-full flex flex-row items-center">
-      <div className="w-3/5">
-        <Hero />
-      </div>
-      <div className="w-2/5"></div>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 };
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,22 @@
 const Hero = () => {
   return (
-    <div className="py-28 text-darkApp">
-      <h1 className="text-6xl font-bold">Go whenever, whenever</h1>
-      <p className="text-xl my-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sit
-        blanditiis ipsum, esse architecto pariatur? Qui unde accusamus voluptas
-        magni quisquam debitis mollitia quia, nobis veniam! Aliquid saepe amet.
-      </p>
-      <button className="py-3 px-10 bg-greenApp hover:bg-darkGreenApp rounded-full">
-        <span className="text-whiteApp font-semibold">Start App</span>
-      </button>
-    </div>
+    <section id="hero" className="w-full flex flex-row items-center">
+      <div className="w-3/5">
+        <div className="py-28 text-darkApp">
+          <h1 className="text-6xl font-bold">Go whenever, whenever</h1>
+          <p className="text-xl my-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+            sit blanditiis ipsum, esse architecto pariatur? Qui unde accusamus
+            voluptas magni quisquam debitis mollitia quia, nobis veniam! Aliquid
+            saepe amet.
+          </p>
+          <button className="py-3 px-10 bg-greenApp hover:bg-darkGreenApp rounded-full">
+            <span className="text-whiteApp font-semibold">Start App</span>
+          </button>
+        </div>
+      </div>
+      <div className="w-2/5"></div>
+    </section>
   );
 };
 
