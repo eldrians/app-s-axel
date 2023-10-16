83400efadf030a77d8501d22124b81880a2c660f
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-10-16 14:56:44 +0700

    app(): add score in Organisasi

--- app/api/form/organisasi/route.ts
+++ app/api/form/organisasi/route.ts
@@ -48,6 +48,58 @@ export async function POST(request: Request) {
     tingkat,
     totalScore,
   } = data;
+  if (data.jabatan == "Ketua") {
+    if (data.tingkat == "Internasional") {
+      data.totalScore = 50;
+    } else if (data.tingkat == "Regional") {
+      data.totalScore = 40;
+    } else if (data.tingkat == "Nasional") {
+      data.totalScore = 30;
+    } else if (data.tingkat == "Provinsi") {
+      data.totalScore = 20;
+    } else if (data.tingkat == "Perguruan Tinggi") {
+      data.totalScore = 10;
+    }
+  } else if (data.jabatan == "Wakil Ketua") {
+    if (data.tingkat == "Internasional") {
+      data.totalScore = 45;
+    } else if (data.tingkat == "Regional") {
+      data.totalScore = 35;
+    } else if (data.tingkat == "Nasional") {
+      data.totalScore = 25;
+    } else if (data.tingkat == "Provinsi") {
+      data.totalScore = 15;
+    } else if (data.tingkat == "Perguruan Tinggi") {
+      data.totalScore = 8;
+    }
+  } else if (
+    data.jabatan == "Sekertaris Umum" ||
+    data.jabatan == "Bendahara Umum"
+  ) {
+    if (data.tingkat == "Internasional") {
+      data.totalScore = 40;
+    } else if (data.tingkat == "Regional") {
+      data.totalScore = 30;
+    } else if (data.tingkat == "Nasional") {
+      data.totalScore = 20;
+    } else if (data.tingkat == "Provinsi") {
+      data.totalScore = 10;
+    } else if (data.tingkat == "Perguruan Tinggi") {
+      data.totalScore = 6;
+    }
+  } else {
+    if (data.tingkat == "Internasional") {
+      data.totalScore = 30;
+    } else if (data.tingkat == "Regional") {
+      data.totalScore = 20;
+    } else if (data.tingkat == "Nasional") {
+      data.totalScore = 10;
+    } else if (data.tingkat == "Provinsi") {
+      data.totalScore = 5;
+    } else if (data.tingkat == "Perguruan Tinggi") {
+      data.totalScore = 2;
+    }
+  }
   try {
     const response = await fetch(
       "https://script.google.com/macros/s/AKfycbwoUI3PSqCUF_xyNMU9vugMuMZ8x1O5JGqy8v5KKfukPpBE7fm0HcKpAKENiBXSxvL8VA/exec",
